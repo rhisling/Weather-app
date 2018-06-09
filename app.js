b3431fbebc5a822e0fc90b69b4e3990472b71416
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-06-09 12:01:10 -0700

    configuring yargs

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs
+  .option({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+console.log(argv);
+var encodedAddress = encodeURIComponent(argv.a);
 
 request(
   {
-    url:
-      "http://maps.googleapis.com/maps/api/geocode/json?address=3191%20dunwich%20court%20california",
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   },
   (error, response, body) => {
-    console.log(body);
+    // console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address : ${body.results[0].formatted_address}`);
+    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
   }
 );
