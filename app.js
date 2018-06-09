ac33b1dc9d3153a7f0aebb2a50602f14dbe38e11
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-06-09 12:13:24 -0700

    Handling error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const yargs = require("yargs");
 const argv = yargs
   .option({
     a: {
-      demand: true,
+      demandOption: true,
       alias: "address",
       describe: "Address to fetch weather for",
       string: true
@@ -12,8 +12,8 @@ const argv = yargs
   })
   .help()
   .alias("help", "h").argv;
-console.log(argv);
-var encodedAddress = encodeURIComponent(argv.a);
+//console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
 
 request(
   {
@@ -21,9 +21,15 @@ request(
     json: true
   },
   (error, response, body) => {
+    if (error) {
+      console.log("Unable to connect to google servers");
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log("unable to find that address");
+    } else if (body.status === "OK") {
+      console.log(`Address : ${body.results[0].formatted_address}`);
+      console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    }
     // console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address : ${body.results[0].formatted_address}`);
-    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
   }
 );
